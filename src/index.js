ba3acb41dbe7dfb8f8ce1a2ab03fd45df0c39b08
Júlia Bacchi <134342345+juliabacchi1@users.noreply.github.com>
2024-06-08 10:38:44 -0300

    Added more weather data JS

--- src/index.js
+++ src/index.js
@@ -1,3 +1,54 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+
+  console.log(response.data.condition.description);
+  console.log(response.data.temperature.humidity);
+  console.log(response.data.wind.speed);
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windElement.innerHTML = `${response.data.wind.speed}km/h`;
+  timeElement.innerHTML = formatDate(date);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hours}:${minutes}`;
+}
+
+//Make API call and update the interface
+function searchCity(city) {
+  //Separation of concern: Thats why I did a new function, to make each function do only one thing, and do it well
+  let apiKey = "4b59c6fof3bta7634229de85af0382e7";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 //Button Submit: Change the name of the city in the HTML, when press the button Search
 
 function handleSearchSubmit(event) {
@@ -6,9 +57,11 @@ function handleSearchSubmit(event) {
   let cityElement = document.querySelector("#city");
 
   cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
-//Show temperature: 
+searchCity("São Paulo");
